966ee7b82a128dd4629acac6d28c175a91ebce0e
josterand <josterand@outlook.com>
2026-04-30 09:12:55 +0700

    in development

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pandas as pd
-
-def main():
-    global df
-    df = pd.read_csv('tumor-data.csv')
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def checkDataSetInfo():
     """
@@ -17,6 +15,25 @@ def checkDataSetInfo():
     print("\n" + "="*50 + "\n", "Sekilas isi file CSV", "\n" + "="*50 + "\n")
     print(df.describe())
 
+def getGraph():
+    print("Memproses visualisasi Data")
+    fig, axes = plt.subplots(1,2,figsize=(12,5))
+
+    # Grafik kiri: Mean Smoothness
+    sns.histplot(df["mean smoothness"], kde=True, color="blue", ax=axes[0])
+    axes[0].set_title("Distribusi Mean Smoothness")
+
+    sns.histplot(df["mean area"], kde=True, color="red", ax=axes[1])
+    axes[1].set_title("Distribusi Mean Area")
+
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    global df
+    df = pd.read_csv('tumor-data.csv')
+
 if __name__ == "__main__":
     main() # Biarin aja di paling pertama!
-    checkDataSetInfo()
+    # checkDataSetInfo()
+    getGraph()
